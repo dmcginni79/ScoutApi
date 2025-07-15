320eb58c38431a12d294f04c1d8719ba1a134107
dmcginni79 <dmcginni@mac.com>
2025-07-15 08:55:04 -0400

    update

---
+++ Scouts/CreateScoutEndpoint.cs
@@ -0,0 +1,41 @@
+using FastEndpoints;
+using ScoutApi.Contracts;
+using ScoutApi.Contracts.Requests;
+using ScoutApi.Contracts.Responses;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ScoutApi.Scouts;
+
+public class CreateScoutEndpoint 
+    : Endpoint<CreateScoutRequest, Results<Created<ScoutResponse>, BadRequest>>
+{
+    private readonly IScoutService _scoutService;
+
+    public CreateScoutEndpoint(IScoutService scoutService)
+    {
+        _scoutService = scoutService;
+    }
+
+    public override void Configure()
+    {
+        Post("/scout");
+        AllowAnonymous();
+    }
+    
+    public override async Task<Results<Created<ScoutResponse>, BadRequest>> 
+        ExecuteAsync(CreateScoutRequest req, CancellationToken ct)
+    {
+        var scout = req.MapToScout();
+        var result = await _scoutService.CreateAsync(scout);
+        var response = result.Match<IResult>(
+            _ => TypedResults.Created($"/scouts/{scout.Id}", scout.MapToResponse()),
+            failed => TypedResults.BadRequest(failed.MapToResponse()));
+
+        return response switch
+        {
+            Created<ScoutResponse> success => success,
+            BadRequest badRequest => badRequest,
+            _ => throw new Exception("Impossible")
+        };
+    }
+}
